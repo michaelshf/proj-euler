78d4f42c0c0c177cc7c40251ec7947b8155de5f8
Michael Simonson <michaels@healthfinch.com>
2018-09-11 09:37:20 -0500

    second example in clojure

--- test/project_euler/core_test.clj
+++ test/project_euler/core_test.clj
@@ -1,9 +1,15 @@
 (ns project-euler.core-test
   (:require [clojure.test :refer :all]
             [project-euler.core :refer :all]
-            [project-euler.ex1 :as ex1]))
+            [project-euler.ex1 :as ex1]
+            [project-euler.ex2 :as ex2]))
 
 
 (deftest example-one
   (testing "example 1 example"
     (is (= 23 (ex1/multiple-summer 10)))))
+
+
+(deftest example-two
+  (testing "example 2 provided example"
+    (is (= 44 (ex2/even-fib-summer 100)))))
