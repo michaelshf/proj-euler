e00fe8db45f69ad280d32ab670d093c693a924de
Michael Simonson <michaels@healthfinch.com>
2019-03-25 21:29:33 -0500

    Reset problems for fun

--- clojure-euler/test/project_euler/core_test.clj
+++ clojure-euler/test/project_euler/core_test.clj
@@ -1,20 +1,4 @@
 (ns project-euler.core-test
   (:require [clojure.test :refer :all]
-            [project-euler.core :refer :all]
-            [project-euler.ex1 :as ex1]
-            [project-euler.ex2 :as ex2]
-            [project-euler.ex3 :as ex3]))
+            [project-euler.core :refer :all]))
 
-
-(deftest example-one
-  (testing "example 1 example"
-    (is (= 23 (ex1/multiple-summer 10)))))
-
-
-(deftest example-two
-  (testing "example 2 provided example"
-    (is (= 44 (ex2/even-fib-summer 100)))))
-
-(deftest example-three
-  (testing "example 3 provided example"
-    (is (= 29 (ex3/biggest-prime-factor-finder 13195)))))
